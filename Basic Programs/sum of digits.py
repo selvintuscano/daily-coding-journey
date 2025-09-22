429a3a609f7dfd0155dab3d48a9b6c3fd91c6aa7
Selvin Tuscano <64688726+selvintuscano@users.noreply.github.com>
2025-09-22 12:01:29 -0400

    Add files via upload

--- Basic Programs/sum of digits.py
+++ Basic Programs/sum of digits.py
@@ -1,7 +1,12 @@
-num = int(input("Enter a number: "))
-sum_digits = 0
+def sum_of_digits(n):
 
-while num > 0:
-    digit = num % 10
-    sum_digits += digit
-    num //= 10
+    total = 0 
+
+    while n > 0:
+        digit=n%10
+        total = total + digit
+        n = n // 10
+
+    return total
+
+print(sum_of_digits(123))
