2b56c9dd4c5c0c6fee27c2f7e1d7775d47fb871c
Selvin Tuscano <64688726+selvintuscano@users.noreply.github.com>
2025-09-25 11:55:24 -0400

    Add files via upload

---
+++ Basic Programs/Count words in a string.py
@@ -0,0 +1,12 @@
+def count_words(text):
+    
+    words = text.split()
+    return len(words)
+
+
+print(count_words("This is Python"))            
+print(count_words("   Extra   spaces   here ")) 
+
+
+
+
