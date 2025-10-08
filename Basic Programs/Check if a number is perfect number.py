921773cbbb851c0d4945f917cbef1ccfe0367041
Selvin Tuscano <64688726+selvintuscano@users.noreply.github.com>
2025-10-08 03:20:41 -0400

    Add files via upload

---
+++ Basic Programs/Check if a number is perfect number.py
@@ -0,0 +1,10 @@
+def is_perfect_number(n):
+    if n <= 0:
+        return False
+    
+    divisor_sum = 0
+    for i in range(1, n):
+        if n % i == 0:
+            divisor_sum += i
+    
+    return divisor_sum == n
