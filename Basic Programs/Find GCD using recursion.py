0ad7c0c6b3a66a689a58baa7453190d02eba9732
Selvin Tuscano <64688726+selvintuscano@users.noreply.github.com>
2025-10-16 22:15:05 -0400

    Add files via upload

---
+++ Basic Programs/Find GCD using recursion.py
@@ -0,0 +1,11 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+# Example usage
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("GCD of", num1, "and", num2, "is:", gcd(num1, num2))
