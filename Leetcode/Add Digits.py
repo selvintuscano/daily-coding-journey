6769ac5289840ebd830bb7630577c4391247b2ac
Selvin Tuscano <64688726+selvintuscano@users.noreply.github.com>
2025-10-26 18:03:56 -0400

    Add files via upload

---
+++ Leetcode/Add Digits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+
+        while num >= 10:
+            sumd = 0 
+
+            while num > 0:
+                digit = num%10
+                sumd = sumd + digit
+                num = num//10
+            num = sumd
+        return num 
