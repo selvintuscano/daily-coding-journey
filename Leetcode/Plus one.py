c86721464fdadd9b9c6b931dee5dd8b53d0d2130
Selvin Tuscano <64688726+selvintuscano@users.noreply.github.com>
2025-10-28 11:13:38 -0400

    Add files via upload

--- Leetcode/Plus one.py
+++ Leetcode/Plus one.py
@@ -1,15 +1,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        # Start from the last digit and move backward
-        for i in range(len(digits) - 1, -1, -1):
-            digits[i] += 1  # add one to the current digit
-            
-            # if the digit is less than 10, no carry -> return result
-            if digits[i] < 10:
-                return digits
-            
-            # if digit becomes 10, set it to 0 and carry 1 to the next iteration
-            digits[i] = 0
-        
-        # if all digits were 9, we need to add a new 1 at the start
-        return [1] + digits
+        if len(digits) == 0:
+            return [1]
+
+        if digits[-1] != 9:
+            digits[-1] += 1
+            return digits
+        else:
+            digits = self.plusOne(digits[:-1])
+            digits.append(0)
+            return digits
