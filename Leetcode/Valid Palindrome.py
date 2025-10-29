7c84e30d6c6673e9dbe7d734d565324e53990b74
Selvin Tuscano <64688726+selvintuscano@users.noreply.github.com>
2025-10-29 11:58:30 -0400

    Add files via upload

---
+++ Leetcode/Valid Palindrome.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        l = 0 
+        r = len(s)-1
+
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            
+            if s[l].upper() != s[r].upper():
+                return False
+
+            l += 1
+            r -= 1
+
+        return True
+            
+
+        
+        
